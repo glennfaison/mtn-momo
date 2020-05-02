d857363eff08c57d30c99c08a523d090f8d51454
glennfaison <glennfaison@gmail.com>
2020-05-02 16:26:23 +0100

    testing: add unit tests for user provisioning

---
+++ src/user-provisioning.spec.js
@@ -0,0 +1,19 @@
+const { describe, it } = require('mocha');
+const { expect } = require('chai');
+
+const momo = require('../src/user-provisioning');
+
+describe('Sandbox User Provisioning', () => {
+  it('should create a sandbox user and api key', async () => {
+    const sandboxUserInfo = await momo.createApiUserAndKey({
+      subscriptionKey: '<your-subscription-key>',
+      providerCallbackHost: '<your-callback-host>'
+    });
+    const { userId, providerCallbackHost, targetEnvironment, apiKey } = sandboxUserInfo;
+
+    expect(userId).to.be.a('string');
+    expect(providerCallbackHost).to.be.a('string');
+    expect(targetEnvironment).to.equal('sandbox');
+    expect(apiKey).to.be.a('string');
+  });
+});
