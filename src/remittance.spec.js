fea04a2b2fe7b5117dc56af08dcbc8236ac5a9f5
glennfaison <glennfaison@gmail.com>
2020-05-07 05:56:07 +0100

    Fix remittances API test

--- src/remittance.spec.js
+++ src/remittance.spec.js
@@ -2,15 +2,15 @@ const { describe, it } = require('mocha');
 const { expect } = require('chai');
 const { config } = require('dotenv');
 
-const { initDisbursements, userProvisioning } = require('.');
+const { initRemittances, userProvisioning } = require('.');
 
 config();
 
-describe('Single run-through for Disbursements API', () => {
+describe.only('Single run-through for Remittances API', () => {
   it('should not throw', async () => {
     // Import package
 
-    const subscriptionKey = process.env.DISBURSEMENTS_PRIMARY_KEY;
+    const subscriptionKey = process.env.REMITTANCES_PRIMARY_KEY;
 
     // (sandbox/development environment only) Provision/create a user and api key
     const sandboxUserInfo = await userProvisioning.createApiUserAndKey({
@@ -20,28 +20,28 @@ describe('Single run-through for Disbursements API', () => {
     const { userId, apiKey, targetEnvironment } = sandboxUserInfo;
 
     // Initialize the wrapper
-    const disbursements = initDisbursements({
+    const remittances = initRemittances({
       subscriptionKey,
       apiKey,
       userId,
       targetEnvironment
     });
 
-    /* Disbursements API */
+    /* Remittances API */
 
     // (optional) Get an access token
-    const token = await disbursements.getToken();
+    const token = await remittances.getToken();
     expect(token).to.have.property('expires_in');
     expect(token.expires_in).to.be.lessThan(Date.now());
 
     // Check if an account is active. Returns a boolean value
-    const isActive = await disbursements.isAccountActive({
+    const isActive = await remittances.isAccountActive({
       accountHolderIdType: 'msisdn',
       accountHolderId: '237675611933'
     });
     expect(isActive).to.be.a('boolean');
 
-    // Approve a request for payment
+    // Approve a remittance/cashout
     const paymentOptions = {
       amount: 15000,
       currency: 'EUR',
@@ -53,20 +53,20 @@ describe('Single run-through for Disbursements API', () => {
       payerMessage: 'message',
       payeeNote: 'note'
     };
-    const transactionId = await disbursements.initiate({
+    const transactionId = await remittances.initiate({
       callbackUrl: 'http://test.com',
       paymentOptions: paymentOptions
     });
     expect(transactionId).to.be.a('string');
 
     // Check the status of a for payment
-    const transaction = await disbursements.fetchTransaction(transactionId);
+    const transaction = await remittances.fetchTransaction(transactionId);
     expect(transaction).to.be.an('object');
 
     // Check my account balance
-    const accountBalance = await disbursements.fetchAccountBalance();
+    const accountBalance = await remittances.fetchAccountBalance();
     expect(accountBalance).to.be.an('object');
 
-    /* End Disbursements API */
+    /* End Remittances API */
   });
 });
